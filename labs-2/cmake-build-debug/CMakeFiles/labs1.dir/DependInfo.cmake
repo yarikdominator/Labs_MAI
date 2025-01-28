afd5f9a769bd0aea70ed953deea1a18fd9631c59
ll337 <123@gmail.com>
2025-01-29 00:31:51 +0300

    add

---
+++ labs-2/cmake-build-debug/CMakeFiles/labs1.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/ll337/labs1/labs-2/while_version.cpp" "CMakeFiles/labs1.dir/while_version.cpp.o" "gcc" "CMakeFiles/labs1.dir/while_version.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ labs-2/cmake-build-debug/CMakeFiles/labs1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/labs1.dir/while_version.cpp.o"
+  "CMakeFiles/labs1.dir/while_version.cpp.o.d"
+  "labs1"
+  "labs1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/labs1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
